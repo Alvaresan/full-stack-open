2abd828f842e0fdc0415a61563aa5f2da1f7f51c
Alvaresan <alvaresan@icloud.com>
2024-02-26 18:58:08 +0100

    part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return <div>No feedback given</div>;
+  } else {
+    return (
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine
+            text="average"
+            value={(good - bad) / (good + neutral + bad)}
+          />
+          <StatisticLine
+            text="positive"
+            value={(good / (good + neutral + bad)) * 100 + " %"}
+          />
+        </tbody>
+      </table>
+    );
+  }
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClickGood = () => {
+    setGood(good + 1);
+  };
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />
+      <Button handleClick={handleClickBad} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+export default App;
